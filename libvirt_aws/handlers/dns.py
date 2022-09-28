9d270e1509cb2aee10a05d034f8ef344fdf33da0
Elvis Pranskevichus <elvis@edgedb.com>
2022-09-28 16:16:15 -0700

    Fix network-update workaround
    Looks like `getVersion` returns a "compatibility version", so check for
    the version of the binding itself.

--- libvirt_aws/handlers/dns.py
+++ libvirt_aws/handlers/dns.py
@@ -420,7 +420,7 @@ def _net_update(
     xml: str,
 ) -> None:
     lvconn = net.connect()
-    if lvconn.getVersion() < 7002000:
+    if lvconn.getLibVersion() < 7002000:
         net.update(section, command, -1, xml)
     else:
         net.update(command, section, -1, xml)
